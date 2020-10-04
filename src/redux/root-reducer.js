2cf52ff0c2b7b016959b1ae5ee0c8238b0e86828
Mehul <mehulsancheti0212@gmail.com>
2020-10-04 15:10:13 -0400

    feat : add-cart-component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 
 import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart-reducer';
 
 //function to create rootReducer object
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer 
 });
