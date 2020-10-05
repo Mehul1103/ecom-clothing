ee693f2e645743238071aab4fe17b764ddd0e758
Mehul <mehulsancheti0212@gmail.com>
2020-10-05 16:43:49 -0400

    feat:add-item-to-cart

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,16 +1,27 @@
 import CartActionTypes from './cart-types';
+import {addItemToCart} from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden : true
+    hidden : true,
+    cartItems : []
 };
 
+//declaring reducer with state and action.
 const cartReducer = (state = INITIAL_STATE, action) => {
+    //looping through kind of action type and work according to it
     switch(action.type){
+        //if the user click on cart-icon, change the hidden state and display cart-dropdown state and pass it to components
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return{
                 ...state,
                 hidden : !state.hidden
             }
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems : addItemToCart(state.cartItems, action.payload)
+            }
+            //default state
             default :
                 return state
     }
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,20 @@
+//declaring a util function with array and an item to add(catch it from when user clicks on add to cart)
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    //it will find if the particular item is already present in cart by checking the id.
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    //if item is present, map the item by checking both id and increase the quantity
+    if(existingCartItem){
+        //map method creates a new array with result being affected on every element of array.
+        //In this, it will increase the count if id matches, otherwise keep the value same  
+        return cartItems.map(cartItem => 
+        cartItem.id === cartItemToAdd.id
+        ? {...cartItem, quantity : cartItem.quantity + 1}
+        : {cartItem}
+        )
+    }
+    //otherwise return the same array and that latest item to add with quantity 1
+    return [...cartItems, {...cartItemToAdd, quantity : 1}]
+}
